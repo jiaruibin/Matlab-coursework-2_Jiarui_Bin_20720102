cbf07258cf4a8149b1a87fa64dee4a7098754177
maxnolima <wimpy.inshore.1f@icloud.com>
2026-04-25 20:38:57 +0800

    Acquire temperature data

--- Jiarui_Bin_20720102.m
+++ Jiarui_Bin_20720102.m
@@ -34,6 +34,7 @@ for i = 1:numBlinks
 end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
+
 % (a) Arduino and thermistor setup
 
 % Analogue pin connected to the output pin of the MCP9700A sensor
@@ -42,10 +43,9 @@ tempPin = 'A0';
 % Location used in the formatted output
 location = 'Nottingham';
 
-% MCP9700A sensor constants
 % V0C is the output voltage at 0 degrees Celsius
-% TC is the temperature coefficient in V/degree Celsius
 V0C = 0.500;
+% TC is the temperature coefficient in V/degree Celsius
 TC = 0.010;
 
 % Test one voltage reading from the temperature sensor
@@ -56,6 +56,57 @@ fprintf('Task 1a test reading:\n');
 fprintf('Voltage = %.3f V\n', testVoltage);
 fprintf('Temperature = %.2f C\n\n', testTemperature);
 
+% (b) Acquire temperature data for 600 seconds
+
+% Total acquisition time in seconds
+duration = 600;
+
+% Sampling interval in seconds
+sampleInterval = 1;
+
+% Number of samples, including time = 0 s and time = 600 s
+numSamples = floor(duration / sampleInterval) + 1;
+
+% Pre-allocate arrays for time, voltage and temperature
+timeData = zeros(numSamples, 1);
+voltageData = zeros(numSamples, 1);
+temperatureData = zeros(numSamples, 1);
+
+% Start timing
+startTimer = tic;
+
+for k = 1:numSamples
+
+    % Target time for this sample
+    targetTime = (k - 1) * sampleInterval;
+
+    % Wait until the target sampling time is reached
+    while toc(startTimer) < targetTime
+        pause(0.01);
+    end
+
+    % Record the actual elapsed time
+    timeData(k) = toc(startTimer);
+
+    % Read voltage from the temperature sensor
+    voltageData(k) = readVoltage(a, tempPin);
+
+    % Convert voltage to temperature using MCP9700A equation
+    temperatureData(k) = (voltageData(k) - V0C) / TC;
+
+    % Print progress every 60 seconds
+    if mod(k - 1, 60) == 0
+        fprintf('Acquired data at %.0f s: %.2f C\n', ...
+            timeData(k), temperatureData(k));
+    end
+end
+
+% Max temperature
+maxTemp = max(temperatureData);
+% Min temperature
+minTemp = min(temperatureData);
+% Average temperature
+avgTemp = mean(temperatureData);
 
 
 %% TASK 2 - LED TEMPERATURE MONITORING DEVICE IMPLEMENTATION [25 MARKS]
