d2169801c43eefb19d359c57622eff08dc2b1cac
maxnolima <wimpy.inshore.1f@icloud.com>
2026-04-25 19:13:06 +0800

    Add LED blinking loop

--- Jiarui_Bin_20720102.m
+++ Jiarui_Bin_20720102.m
@@ -12,11 +12,26 @@ ledPin = 'D8';
 
 % Switch LED ON
 writeDigitalPin(a, ledPin, 1);
-pause(1);
+pause(1)
 
 % Switch LED OFF
 writeDigitalPin(a, ledPin, 0);
+pause(1)
+% Number of blinking cycles
+numBlinks = 10;
 
+% Blink the LED at 0.5 s intervals
+for i = 1:numBlinks
+
+    % Switch LED ON
+    writeDigitalPin(a, ledPin, 1);
+    pause(0.5);
+
+    % Switch LED OFF
+    writeDigitalPin(a, ledPin, 0);
+    pause(0.5);
+
+end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
