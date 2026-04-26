74dda241905e153e72dff5e3af5426f49b72a77f
maxnolima <wimpy.inshore.1f@icloud.com>
2026-04-26 19:20:54 +0800

    final result

--- Jiarui_Bin_20720102.m
+++ Jiarui_Bin_20720102.m
@@ -194,7 +194,7 @@ writeDigitalPin(a, yellowPin, 0);
 writeDigitalPin(a, redPin, 0);
 
 % Call the temperature monitoring function
-temp_monitor(a, tempPin, greenPin, yellowPin, redPin,20,22);
+temp_monitor(a, tempPin, greenPin, yellowPin, redPin,18,24);
 
 
 
